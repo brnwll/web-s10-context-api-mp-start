5d54b75dbf8589f061e897754ed7641a29e9ac0e
Brian Howell <brianhowell@pm.me>
2024-01-17 13:14:51 -0500

    QuotesProvider implemented, but not sure if working yet

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,15 @@
-import './styles/reset.css'
-import './styles/styles.css'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App'
+import "./styles/reset.css";
+import "./styles/styles.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { QuotesProvider } from "./context/quotesContext";
+import App from "./components/App";
 
-const domNode = document.getElementById('root')
-const root = createRoot(domNode)
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
 root.render(
-  <App />
-)
+  <QuotesProvider>
+    <App />
+  </QuotesProvider>
+);
